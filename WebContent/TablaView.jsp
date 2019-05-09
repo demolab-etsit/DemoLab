6fa3702bc249d3e202db2ef350b7e8420d0d5087
lmagellanic-cloud <carlos.moreno.miguel@alumnos.upm.es>
2019-05-09 23:24:18 +0200

    Rellenar Tabla con resultados 2019

--- WebContent/TablaView.jsp
+++ WebContent/TablaView.jsp
@@ -14,62 +14,41 @@
 </head>
 <body>
 	<h1>Simulación</h1>
-	
-	
-<h3>Resultados de elecciones generales anteriores</h3>		
-<form name="link">
-	<select name="menu">
-		<option selected="selected">Selecciona unos resultados electorales</option>
-		<option value="https://resultados.elpais.com/elecciones/2019/generales/congreso/index.html">Elecciones generales 2019</option>
-		<option value="https://resultados.elpais.com/elecciones/2016/generales/congreso/index.html">Elecciones generales 2016</option>
-		<option value="https://resultados.elpais.com/elecciones/2015/generales/congreso/index.html">Elecciones generales 2015</option>
-		<option value="https://resultados.elpais.com/elecciones/2011/generales/congreso/index.html">Elecciones generales 2011</option>
-		<option value="http://www.infoelectoral.mir.es/infoelectoral/min/">Elecciones anteriores</option>
-
-	</select> 
-		 <input onclick="location=document.link.menu.options[document.link.menu.selectedIndex].value;" type="button" value="Ir" /><span style="line-height: 1.3em;"> </span>
- </form>
-	
-	
-	
 	<h3>Inserte el número de votos en la tabla</h3>
 	<form method="POST" action="DHondtServlet">
-			<table>
-		
-		  <tr>
-		
-		    <th scope="col">Votos</th>
-		    
-			<c:forEach items="${partido_list}" var="part">
-			
-			    <th>${part.acronimo}</th>
-		    
+		<table>
+
+			<tr>
+				<th scope="col">Votos</th>
+				<c:forEach items="${partido_list}" var="part">
+					<th>${part.acronimo}</th>
+				</c:forEach>
+			</tr>
+
+			<c:forEach items="${circunscripcion_list}" var="circ">
+				<tr>
+					<th>${circ.nombre}</th>
+					<c:forEach items="${partido_list}" var="part">
+						<c:forEach items="${votaciones }" var="vota">
+								<c:if
+									test="${ vota.circunscripcion.nombre == circ.nombre && vota.partido.acronimo == part.acronimo }">
+									<td>
+									<input type="number" name="${part.acronimo}${circ.nombre}" min="0"
+										max="${vota.circunscripcion.NElectores }"
+										value="${vota.nvotosString }" />
+									</td>
+								</c:if>
+						</c:forEach>
+					</c:forEach>
+				</tr>
 			</c:forEach>
-		  </tr>
-		
-		
-		<c:forEach items="${circunscripcion_list}" var="circ">
-		
-		  <tr>
-		
-		    <th>${circ.nombre}</th>
-		    <c:forEach items="${partido_list}" var="part">
-			
-			    <td><input type="number" name="${part.acronimo}${circ.nombre}" min=0 value=10></td>
-			    
-		    
-			</c:forEach>
-		
-		
-		  </tr>
-		</c:forEach>
-		 
-		
+
+
 		</table>
-		
+
 		<a class="button" href="IndexView.jsp">Volver</a>
-		
+
 		<button class="button" type="submit">Simular</button>
-		</form>
+	</form>
 </body>
 </html>
--- src/es/isst/demolab/servlets/TablaServlet.java
+++ src/es/isst/demolab/servlets/TablaServlet.java
@@ -1,5 +1,6 @@
 package es.isst.demolab.servlets;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
 
 import es.isst.demolab.dao.CircunscripcionDAO;
 import es.isst.demolab.dao.CircunscripcionDAOImplementation;
@@ -15,8 +20,11 @@ import es.isst.demolab.dao.EleccionDAOImplementation;
 import es.isst.demolab.dao.PartidoDAO;
 import es.isst.demolab.dao.PartidoDAOImplementation;
 import es.isst.demolab.model.Circunscripcion;
+import es.isst.demolab.model.Elec_Circ_Part;
 import es.isst.demolab.model.Partido;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,11 +45,96 @@ public class TablaServlet extends HttpServlet{
 		
 //		Map<Partido, Map<Circunscripcion, Integer>> votos = new HashMap<Partido,Map<Circunscripcion,Integer>>();
 		
+		Collection<Partido> partidos = PartidoDAOImplementation.getInstancia().readAll();
+		Collection<Circunscripcion> circunscripciones = CircunscripcionDAOImplementation.getInstancia().readAll();
+		Collection<Elec_Circ_Part> votaciones = new ArrayList();
+
+		String urlToXML = this.getServletContext().getRealPath("/WEB-INF/xml/2019/");  
+		File xmlDir = new File(urlToXML);
+		File[] xmlFiles = xmlDir.listFiles();
 		
+		System.out.println("\n Working directory(TablaServlet) = "+ urlToXML + "\n");
+
+		//Inicializacion para leer el XML
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = null;
+		Document document = null;
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			for(File seleccionada : xmlFiles) {
+				document = documentBuilder.parse(seleccionada);
+				String nombreProvincia = seleccionada.getName();
+				nombreProvincia = nombreProvincia.replaceAll(".xml", "");
+				Circunscripcion circSeleccionada = null;
+				for(Circunscripcion tempCirc : circunscripciones) {
+//					System.out.println("tempCirc.getNombre()=" + tempCirc.getNombre()
+//					+ "==nombreProvincia=" + nombreProvincia +"\n");
+					if(tempCirc.getNombre().equals(nombreProvincia)) {
+//						System.out.println("TablaServlet tempCirc encontrada:\n");
+//						System.out.println("tempCirc.getNombre() = " + tempCirc.getNombre()
+//						+ " == nombreProvincia " + nombreProvincia +"\n");
+						circSeleccionada = tempCirc;
+					}
+				}
+				
+//				System.out.println("El nombre de la provincia del XML seleccionado es = " + nombreProvincia + "\n");
+				int numeroPartidos = document.getElementsByTagName("nombre").getLength();
+				String[] nombrePartido = new String[numeroPartidos];
+				String[] numVotosDelPartido = new String[numeroPartidos];
+				for(int i = 0; i<numeroPartidos; i++) {
+					nombrePartido[i] =  document.getElementsByTagName("nombre").item(i).getTextContent();
+					numVotosDelPartido[i] = document.getElementsByTagName("votos_numero").item(i).getTextContent(); 
+					if(nombrePartido[i].equals("C's")) {
+						nombrePartido[i] = "Cs";
+					}
+					if(nombrePartido[i].contains("PODEMOS")) {
+						nombrePartido[i] = "PODEMOS";
+					}
+				}
+
+				Partido partSeleccionado = null;
+				for(Partido tmpPartido : partidos) {
+					//						System.out.println("tempPartido.getAcronimos()=" + tmpPartido.getAcronimo()
+					//						+ "==nombrePartido=" + nombrePartido +"\n");
+					int j = 0;
+					boolean partidoEncontrado = false;
+					for(String nombrePartidoSingle : nombrePartido) {
+						if(tmpPartido.getAcronimo().equals(nombrePartidoSingle)) {
+							System.out.println("TablaServlet tempPartido encontrado:\n");
+							System.out.println("tempPartido.getAcronimos()=" + tmpPartido.getAcronimo()
+							+ "==nombrePartido=" + nombrePartidoSingle +"\n");
+							partSeleccionado = tmpPartido;
+
+							Elec_Circ_Part elec_circ_part_i = new Elec_Circ_Part();
+							elec_circ_part_i.setCircunscripcion(circSeleccionada);
+							elec_circ_part_i.setPartido(partSeleccionado);
+							elec_circ_part_i.setNVotos(Integer.parseInt(numVotosDelPartido[j]));
+							votaciones.add(elec_circ_part_i);
+							partidoEncontrado = true;
+						}
+						j++;
+					}
+					if(!partidoEncontrado) {
+						Elec_Circ_Part elec_circ_part_i = new Elec_Circ_Part();
+						elec_circ_part_i.setCircunscripcion(circSeleccionada);
+						elec_circ_part_i.setPartido(tmpPartido);
+						elec_circ_part_i.setNVotos(0);
+						votaciones.add(elec_circ_part_i);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-//		req.getSession().setAttribute("votos", votos);
-		
-		
+		for(Elec_Circ_Part i : votaciones) {
+			System.out.println("Provincia= " + i.getCircunscripcion().getNombre() + "\n");
+			System.out.println("\tEl partido " + i.getPartido().getAcronimo() + " ha obtenido "
+					+ i.getNvotosString() + " votos\n");
+		}
+		req.getSession().setAttribute("votaciones", votaciones);
+		req.getSession().setAttribute( "partido_list", partidos );
+		req.getSession().setAttribute( "circunscripcion_list", circunscripciones );
 		getServletContext().getRequestDispatcher( "/TablaView.jsp" ).forward( req, resp );
 	}
 }
